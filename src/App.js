a45d578cf715e8ae9a0eda0e90ac9f6baf77b948
doheeMac <thsehgml37@korea.ac.kr>
2022-12-06 15:44:40 +0900

    feat: 로또 번호 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 const InputView = require('./UI/InputView.js');
 const Validator = require('./utils/Validator.js');
+const Lotto = require('./Lotto.js');
+const LottoLibrary = require('./utils/LottoLibrary.js');
 
 class App {
   #lottoTotal;
+  #lotto;
+  #winLottos;
 
   play() {
     this.payLotto();
@@ -12,7 +16,14 @@ class App {
     InputView.readPayment((payment) => {
       const PAYMENT = Validator.paymentValidate(payment);
       this.#lottoTotal = PAYMENT / 1000;
+      return this.purchaseLotto();
     });
+  }
+
+  purchaseLotto() {
+    this.#winLottos = LottoLibrary.generateLotto(this.#lottoTotal);
+
+    //this.#lotto = new Lotto(this.#lottoTotal);
   }
 }
 
---
+++ src/utils/LottoLibrary.js
@@ -0,0 +1,20 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const LottoLibrary = {
+  generateLotto(lottoTotal) {
+    const LOTTO_NUMBERS = [];
+    for (let index = 0; index < lottoTotal; index++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      LOTTO_NUMBERS.push(numbers);
+    }
+    return LOTTO_NUMBERS;
+  },
+
+  //   sortLotto(lottoNumbers) {
+  //     lottoNumbers.map((element) => {
+  //       element.sort();
+  //     });
+  //   },
+};
+
+module.exports = LottoLibrary;
